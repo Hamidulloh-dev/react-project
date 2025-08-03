083bb19ee303d7ee3e20115898bcafdef8be24c7
hamidulloh-dev <hamidullohmuhammadjonov123@gmail.com>
2025-08-03 18:12:28 +0500

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
   /** @type {import('tailwindcss').Config} */
   export default {
-     content: [
-        "./src/**/*.{html,js}",
-        "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
     ],
         theme: {
           extend: {},
